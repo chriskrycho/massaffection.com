fe61b694a2c2f45d2a8ff6d61812095f80806f66
Chris Krycho <chris@chriskrycho.com>
2017-01-29 17:24:06 -0500

    Use method-agnostic links.

--- site/publishconf.py
+++ site/publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'https://www.massaffection.com'
+SITEURL = '//www.massaffection.com'
 RELATIVE_URLS = False
 
 DELETE_OUTPUT_DIRECTORY = True
